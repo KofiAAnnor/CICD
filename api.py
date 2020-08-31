0e2a86b2b4716b12c26d0ab83a0d7b286699a0ce
Kofi Annor <kofiannor@MacBook-2.local>
2020-08-30 21:49:41 -0400

    started api and few tests

--- api.py
+++ api.py
@@ -3,12 +3,48 @@ from flask import Flask, request, jsonify
 # Initializing app and database
 app = Flask(__name__)
 database = {}
+genders = ['male', 'female', 'other']
+year = ['freshman'. 'sophmore', 'junior', 'senior']
 
 # Serves GET and POST requests for API
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    return jsonify(database)
+@app.route("/students", methods=['GET'])
+def getAllStudents():
+    return jsonify(database), 200
 
+@app.route("/student/<uid>", methods=['GET'])
+def getStudent(uid):
+    student = database.get(uid)
+    if student is None:
+        return 404
+    else:
+        return jsonify(student), 200
+
+@app.route("/add", methods=['POST'])
+def addStudent():
+    for student in request.json:
+        if database.get(student['uid']) != None:
+            return 400
+    for student in request.json:
+        uid = student['uid']
+        name = student['name']
+        gender = student['gender']
+        gpa = student['gpa']
+        studentEntry = {uid=uid, name=name, gender=gpa, gpa=gpa}
+        database[uid] = studentEntry
+    return 200
+"""
+@app.route("/delete/<uid>", methods=['DELETE'])
+def deleteStudent(uid):
+    student = database.get(uid)
+    if student is None:
+        return 404
+    else:
+        return database.popitem(uid), 200
+
+@app.route("/update/<uid>", methods=['PUT'])
+def updateStudent(uid):
+    return jsonify(database), 200
+"""
 # Running application
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 from api import app
 import unittest
+from flask import json
+
 
 class Tests(unittest.TestCase):
 
-    def sample_test(self):
+    def addTest(self):
         tester = app.test_client(self)
-        self.assertEqual(400, 400)
+        response = tester.get('/students', content_type='html/text')
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+#        tester = app.test_client(self)
+ #       self.assertEqual(400, 400)
 
 if __name__ == "__main__":
     unittest.main()
