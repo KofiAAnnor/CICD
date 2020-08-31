440c5ef9ef142a8857cb06cbd9e0540cb03426a6
Kofi Annor <kofiannor@MacBook-2.local>
2020-08-31 17:11:55 -0400

    tests 19

--- test.py
+++ test.py
@@ -60,6 +60,6 @@ class Tests(unittest.TestCase):
         # test getStudent empty
         response = tester.get('/student/1267893', content_type='html/text')
         self.assertEqual(response.status_code, 404)
-        '''
+
 if __name__ == "__main__":
     unittest.main()
