89c957f8ebf106594a17c7d8885b5f20f91a1008
Kofi Annor <kofiannor@MacBook-2.local>
2020-08-31 11:03:12 -0400

    test logs 5

--- test.py
+++ test.py
@@ -11,8 +11,7 @@ class Tests(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/students', content_type='html/text')
         print(response)
-        print(response[1])
-        print(response[2])
+        print(response.json)
         #self.assertEqual(response.status_code, 200)
 
 #        tester = app.test_client(self)
