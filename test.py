283297928a522c213fac2d7142ea8c572df44e76
Kofi Annor <kofiannor@MacBook-2.local>
2020-08-31 10:24:14 -0400

    test logs 1

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from api import app
 import unittest
 from flask import json
+import logging
+
 
 
 class Tests(unittest.TestCase):
 
     def addTest(self):
+        logging.warning( "test log" )
         tester = app.test_client(self)
         response = tester.get('/students', content_type='html/text')
-        print(response)
+        print("hello ther")
         self.assertEqual(response.status_code, 200)
 
 #        tester = app.test_client(self)
