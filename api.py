34c5cca2c4c3a13b4ff1af51c27bb394f6fb1b4d
Kofi Annor <kofiannor@MacBook-2.local>
2020-09-14 13:05:13 -0400

    added boto client region

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ database = {}
 genders = ['male', 'female', 'other']
 years = ['freshman', 'sophmore', 'junior', 'senior']
 
-client = boto3.client('secretsmanager')
+client = boto3.client('secretsmanager', region_name='us-west-2')
 response = client.get_secret_value(
     SecretId = 'dbsecrets'
 )
