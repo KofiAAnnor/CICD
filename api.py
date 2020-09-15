1cb183c3cff91f7a4e9e33c82d8c5a08a7da0388
Kofi Annor <kofiannor@MacBook-2.local>
2020-09-14 20:42:16 -0400

    added real database

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import boto3
 
 # Initializing app and database
 app = Flask(__name__)
-database = {}
+
 genders = ['male', 'female', 'other']
 years = ['freshman', 'sophmore', 'junior', 'senior']
 
@@ -19,26 +19,52 @@ mydb = mysql.connector.connect(
   host=secretDict['host'],
   user=secretDict['username'],
   password=secretDict['password']
+  data
 )
-print(mydb)
 
+def studentExists(uid):
+    sql = "SELECT * FROM students WHERE uid = %s"
+    val = (uid,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchone()
+    if myresult:
+        return True
+    else:
+        return False
 
 # Serves GET and POST requests for API
 @app.route("/", methods=['GET'])
 def getHomePage():
-    return "<h1>Welcome to the student API *db copnnected*</h1>", 200
+    return "<h1>THIS IS A TEST API</h1>", 200
 
 @app.route("/students", methods=['GET'])
 def getAllStudents():
-    return jsonify(database), 200
+    sql = "SELECT * FROM students"
+    mycursor.execute(sql)
+
+    row_headers = [x[0] for x in mycursor.description]
+    myresult = mycursor.fetchall()
+    json_data = []
+
+    for result in myresult:
+        json_data.append(dict(zip(row_headers,result)))
+        
+    return jsonify(json_data), 200 
 
 @app.route("/student/<uid>", methods=['GET'])
 def getStudent(uid):
-    student = database.get(int(uid))
-    if student is None:
+    if not studentExists(uid):
         return {}, 404
-    else:
-        return jsonify(student), 200
+
+    sql = "SELECT * FROM students WHERE uid = %s"
+    val = (uid, )
+    mycursor.execute(sql, val)
+    row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+    myresult = mycursor.fetchall()
+    json_data =[]
+    for result in myresult:
+        json_data.append(dict(zip(row_headers,result)))
+    return jsonify(json_data[0]), 200
 
 @app.route("/add", methods=['POST'])
 def addStudent():
@@ -48,21 +74,26 @@ def addStudent():
     gender = student['gender']
     gpa = student['gpa']
     year = student['year']
-
-    if database.get(uid) is None and gender in genders and year in years:
-        studentEntry = {"uid": uid, "name": name, "gender": gender, "gpa": gpa, "year" : year}
-        database[uid] = studentEntry
-        return {}, 200
+    if not studentExists(uid) and gender in genders and year in years:
+        sql = "INSERT INTO students (uid, name, gender, gpa, year) VALUES (%s, %s, %s, %s, %s)"
+        val = (uid, name, gender, gpa, year)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return jsonify({'success':True}), 200
     else:
         return {}, 400
 
 @app.route("/delete/<uid>", methods=['DELETE'])
 def deleteStudent(uid):
-    student = database.get(int(uid))
-    if student is None:
-        return {}, 404
+    if  studentExists(uid):
+        sql = "DELETE FROM students WHERE uid = %s"
+        val = (uid, )
+        mycursor.execute(sql, val)        
+        mydb.commit()
+        return jsonify({'success':True}), 200
     else:
-        return database.pop(int(uid)), 200
+        return {} , 404
+        
 
 # Running application
 if __name__ == "__main__":
