7e04d04e7cb817498fdad42c2ef6f3e69b2d9951
Kofi Annor <kofiannor@MacBook-2.local>
2020-09-18 13:57:50 -0400

    os vars test

--- api.py
+++ api.py
@@ -48,11 +48,11 @@ def studentExists(uid):
 # Serves GET and POST requests for API
 @app.route("/", methods=['GET'])
 def getHomePage():
-    return "<h1>THIS IS A TEST API2 we in der</h1>", 200
+    return "<h1>THIS IS A TEST APItest os vars</h1>", 200
 
 @app.route("/students", methods=['GET'])
 def getAllStudents():
-    return "<h1>GET STUDENTS</h1>", 200
+    return os.environ['TEST'], 200
     '''
     sql = "SELECT * FROM students"
     mycursor.execute(sql)
